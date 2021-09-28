c2d6a8115fc3c7a1df228d76a7bbb2d1f8be4035
Hugo <blackfoxmmm@gmail.com>
2021-09-28 15:48:29 +0500

    ok

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,36 @@ import "./Footer.scss";
 function Footer() {
   return (
     <>
-      <div className="footer__container"></div>
+      <div className="footer__container">
+        <div className="footer__loader"></div>
+        <div className="footer__div">
+          <h2 className="footer__header">
+            Want to share an idea let's discuss
+          </h2>
+
+          <a
+            className="footer__email"
+            href="mailto:abdulmaliikalandarov@gmail.com"
+          >
+            abdulmaliikalandarov@gmail.com
+          </a>
+        </div>
+
+        <ul className="footer__list">
+          <li className="footer__item-telegram">
+            <a href="index.html">telegram</a>
+          </li>
+          <li className="footer__item-linkedin">
+            <a href="index.html">linkedin</a>
+          </li>
+          <li className="footer__item-behance">
+            <a href="index.html">behance</a>
+          </li>
+          <li className="footer__item-dribbble">
+            <a href="index.html">dribbble</a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
