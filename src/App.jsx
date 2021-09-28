87ffec8f35f6d3baf0b322a7c495f397e0cdc599
Hugo <blackfoxmmm@gmail.com>
2021-09-28 15:26:00 +0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
+      <Footer></Footer>
       <Header></Header>
     </>
   );
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer__container"></div>
+    </>
+  );
+}
+
+export default Footer;
