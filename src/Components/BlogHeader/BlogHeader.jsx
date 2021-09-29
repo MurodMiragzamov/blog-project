7a21a841735f7bb813a70c81f72228b62a6dfd79
Hugo <blackfoxmmm@gmail.com>
2021-09-29 16:21:19 +0500

    ever

--- src/Components/BlogHeader/BlogHeader.jsx
+++ src/Components/BlogHeader/BlogHeader.jsx
@@ -5,7 +5,7 @@ function BlogHeader() {
   return (
     <>
       <div className="blogHeader__container">
-        <div className="loader_"></div>
+        <div className="loader_ load"></div>
         <h2 className="blogHeader__header">*some of my works</h2>
       </div>
     </>
