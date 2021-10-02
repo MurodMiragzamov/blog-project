291742e3bea476e10f91766de15598ad2d9f9001
Hugo <blackfoxmmm@gmail.com>
2021-10-02 18:59:44 +0500

    Tg section started

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import PortHeader from "./Components/PortHeader/PortHeader";
 import BlogHeader from "./Components/BlogHeader/BlogHeader";
+import TelegramLink from "./Components/TelegramLink/TelegramLink";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header></Header>
       <PortHeader></PortHeader>
       <BlogHeader></BlogHeader>
+      <TelegramLink></TelegramLink>
     </>
   );
 }
---
+++ src/Components/TelegramLink/TelegramLink.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./TelegramLink.scss";
+
+function TelegramLink() {
+  return (
+    <>
+      <div className="telegramLink__container">
+        <h3 className="link-header">
+          Interesting stories from my life field facts just as many interesting
+          things you can find here
+        </h3>
+
+        <a href="https://t.me/anvarkhani" className="tg-link">
+          This is the longest button link to my telegram channel where you can
+          find a lot of interesting things
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default TelegramLink;
