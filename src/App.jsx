2ce103e9e5772030b7b1a99df713a75584a7124b
Hugo <blackfoxmmm@gmail.com>
2021-10-02 16:43:01 +0500

    blogHeader complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import PortHeader from "./Components/PortHeader/PortHeader";
+import BlogHeader from "./Components/BlogHeader/BlogHeader";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Footer></Footer>
       <Header></Header>
       <PortHeader></PortHeader>
+      <BlogHeader></BlogHeader>
     </>
   );
 }
